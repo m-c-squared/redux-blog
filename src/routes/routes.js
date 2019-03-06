c533c625f0df9471eeafac8701ce80073cc38c7b
pickuppatterns <pickup.patterns@gmail.com>
2019-03-06 14:27:35 -0800

    adds router routes and home page displays on browser

---
+++ src/routes/routes.js
@@ -0,0 +1,41 @@
+import Home from '../container/Home';
+// import FilteredPosts from './container/';
+// import RegisterUser from './container/';
+// import AllUsers from './container/';
+// import UserDetail from './container/';
+// import PostDetail from './container/';
+
+export const ROUTES = {
+  HOME:{
+    path:'/',
+    Component: Home,
+    linkTo:() => '/'
+  } 
+  // FILTERED_POSTS:{
+  //   path:'/',
+  //   Component: FilteredPosts,
+  //   linkTo:() => '/filteredPosts'
+  // },
+  // REGISTERED_USER:{
+  //   path:'/',
+  //   Component: RegisterUser,
+  //   linkTo:() => '/register'
+  // }, 
+  // ALL_USERS:{
+  //   path:'/',
+  //   Component: AllUsers,
+  //   linkTo:() => '/users/allUsers'
+  // }, 
+  // USER_DETAIL:{
+  //   path:'/',
+  //   Component: UserDetail,
+  //   linkTo:() => '/user/detail'
+  // },
+  // POST_DETAIL:{
+  //   path:'/',
+  //   Component: PostDetail,
+  //   linkTo:() => '/post/postDetail'
+  // }
+
+
+};
