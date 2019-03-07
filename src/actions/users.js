403165f66f4f19160127f893d80c6f00c0abbc24
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-06 16:56:30 -0800

    Action: Users test passing

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,7 +1,13 @@
-import { getUsers } from '../services/blogApiServices';
+import { getUsers, getUser } from '../services/blogApiServices';
 
 export const FETCH_USERS = 'FETCH_USERS';
 export const fetchUsers = () => ({
   type: FETCH_USERS,
   payload: getUsers()
+});
+
+export const FETCH_USER = 'FETCH_USER';
+export const fetchUser = (id) => ({
+  type: FETCH_USER,
+  payload: getUser(id)
 });
