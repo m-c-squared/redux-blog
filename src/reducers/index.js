cec707595a81ea7f29f865054bb8c403e8070dc9
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-06 16:22:39 -0800

    combine reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+import { combineReducers } from 'redux';
+import users from './users';
+import posts from './posts';
+import comments from './comments';
+
+export default combineReducers({
+  users,
+  posts,
+  comments
+});
