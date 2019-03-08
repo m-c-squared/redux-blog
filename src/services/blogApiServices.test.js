4f8276d94f19d70946353a882bd75c586444a8ab
Carmen V Ramos <carmenvramos@yahoo.com>
2019-03-08 11:24:56 -0800

    getUserPosts  blog servicesAPI test added & passing

--- src/services/blogApiServices.test.js
+++ src/services/blogApiServices.test.js
@@ -1,4 +1,4 @@
-const { getUser, getUsers, getPosts, getComments } = require('./blogApiServices');
+const { getUser, getUsers, getPosts, getComments, getUserPosts } = require('./blogApiServices');
 
 jest.mock('./blogApiServices');
 
@@ -70,4 +70,26 @@ describe('blogApi services', () => {
         ]);
       });
   });
+
+
+  it('gets posts by user id', () => {
+    return getUserPosts()
+      .then(posts => {
+        expect(posts).toEqual([
+          {
+            userId: 2,
+            id: 11,
+            title: 'et ea vero quia laudantium autem',
+            body: 'delectus reiciendis molestiae occaecati non minima eveniet qui voluptatibus\naccusamus in eum beatae sit\nvel qui neque voluptates ut commodi qui incidunt\nut animi commodi'
+          },
+          {
+            userId: 2,
+            id: 12,
+            title: 'in quibusdam tempore odit est dolorem',
+            body: 'itaque id aut magnam\npraesentium quia et ea odit et ea voluptas et\nsapiente quia nihil amet occaecati quia id voluptatem\nincidunt ea est distinctio odio'
+          }
+        ]);
+      });
+  });
+
 });
