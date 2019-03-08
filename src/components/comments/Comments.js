0d9c390b41837559102d5804db1266cf84485932
Marcy Silverman <marcysilve225@gmail.com>
2019-03-07 17:02:50 -0800

    comments are showing

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+// import User from './User';
 // import { Link } from 'react-router-dom';
-// import { ROUTES }   from '../../routes/routes';
+// import { ROUTES } from '../../routes/routes';
 
 const Comments = ({ comments }) => {
-  const commentList = comments.map(({ id }) => {
-    return (<li key={id}>{id}</li>);
-  });
 
-  // const commentList = comments.map(({ commentId }) => 
-  //   <Link key={ commentId} to={`/comments/${commentId}`}></Link>);
+  const commentList = comments.map(({ postId, body }) => {
+    return <li key={postId}> {body} </li> ;
+  });
+  // const commentList = comments.map(({ postId, body }) => 
+  //   <Link key={postId} to={ROUTES.USER_CONTAINER.linkTo(postId)}> { body } </Link>);
   return (
-      <>
-      <ul> 
-        {commentList}
-      </ul>
-      </>
+    <>
+    <ul>
+      {commentList}
+    </ul>
+     </>
   );
 };
 
-Comments.propTypes = {
-  comments: PropTypes.string.isRequired
+Comments.propTypes =  {
+  comments: PropTypes.array.isRequired
 };
 
-export default Comments;
+export default Comments; 
