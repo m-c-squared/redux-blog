25c3f5251e4d2495c1aa892724980696af853280
Michael Elliott <pickup.patterns@gmail.com>
2019-03-08 13:11:16 -0800

    Snapshooter (#25)
    * app compnent snapshot test passing
    
    * comment compnent snapshot test passing
    
    * header componet snapshot test passing
    
    * adds or updates test naming convention, all snapshot test passing
    
    * updates snapshots for travis approval
    
    * updates postDetail
    
    * intentionally fail snapshots to trigger git to add to build
    
    * updates snapshots
    
    * removes snapshots
    
    * adds snapshot tests

--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom';
 import { ROUTES } from '../../routes/routes';
 
 const Posts = ({ posts }) => {
-  const postList = posts.map(({ id, title }) => 
-    <Link key={id} to={ROUTES.POST_CONTAINER.linkTo(id)}> {title} </Link>);
-
-  // const postList = posts.map(({ id, title }) => {
-  //   return <li key={id}> {title} </li>;
-  // });
+  const postList = posts.map((post, i) => {
+    return (
+      <li key={i}>
+        <Link key={post.id} to={ROUTES.POST_CONTAINER.linkTo(post.id)}>
+          <p>{post.title}</p>
+        </Link>
+        <p>{post.body}</p>
+      </li>
+    );
+  });
 
   return (
     <>
