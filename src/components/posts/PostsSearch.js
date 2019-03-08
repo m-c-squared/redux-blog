3b9c70d231ec6933fd44c487eeb69ebb91475b75
Cari Palazzolo <cari.pizza@gmail.com>
2019-03-08 07:18:01 -0800

    added search components

---
+++ src/components/posts/PostsSearch.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import SearchTerm from '../../container/posts/SearchTerm';
+import FilteredPosts from '../../container/posts/FilteredPosts';
+
+const PostsSearch = () => {
+  return (
+    < >
+      <SearchTerm />
+      <FilteredPosts />
+    </>
+  );
+};
+
+export default PostsSearch;
