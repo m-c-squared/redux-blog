d1a16e2c9a643feea3b2c8d1bb5ca0910cdb3910
Marcy Silverman <marcysilve225@gmail.com>
2019-03-07 17:23:35 -0800

    things are a workin

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-// import User from './User';
-// import { Link } from 'react-router-dom';
-// import { ROUTES } from '../../routes/routes';
 
 const Comments = ({ comments }) => {
 
-  const commentList = comments.map(({ postId, body }) => {
-    return <li key={postId}> {body} </li> ;
-  });
-  // const commentList = comments.map(({ postId, body }) => 
-  //   <Link key={postId} to={ROUTES.USER_CONTAINER.linkTo(postId)}> { body } </Link>);
+  const commentList = comments.map((({ body }) => {
+    return <li key={body}> {body} </li>;
+  }));
+
   return (
     <>
     <ul>
